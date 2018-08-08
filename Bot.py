c8f645feefe0034306bdab6351731fae0884b425
AndreaErario <42212286+AndreaErario@users.noreply.github.com>
2018-08-08 16:33:03 +0200

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+
+Client = discord.Client()  
+client = commands.Bot(command_prefix="!")
+
+
+@client.event 
+async def on_ready():
+    print("Il Bot e' ora operativo")
+
+@client.event
+async def on_message(message):
+    
+    if message.content.upper().startswith('!COOKIE'):
+        try:
+            await client.delete_message(message)
+            await client.send_message(message.channel, ":cookie:")
+        except discord.errors.NotFound:
+            return
+    
+    
+    if message.content.upper().startswith('!SAY'):
+        if "role" in [role.name for role in message.author.roles]:
+            args = message.content.split(" ")
+            try:
+                await client.delete_message(message)
+                await client.send_message(message.channel,"%s " % (" ".join(args[1:])))
+            except discord.errors.NotFound:
+                return
+        else:
+            await client.send_message(message.channel, "Scusa amico, non hai il permesso")
+
+
+client.run("TOKEN")
