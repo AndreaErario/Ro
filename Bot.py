717af63cc7dfddec9156ff04fc4212571750443b
AndreaErario <42212286+AndreaErario@users.noreply.github.com>
2018-08-11 11:25:00 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -53,7 +53,7 @@ async def on_message(message):
 
     
     if message.content.upper().startswith("!CLEAR"):
-        if "Admin Bot" in [role.name for role in message.author.roles]:
+        if "Bot Admin" in [role.name for role in message.author.roles]:
             tmp = await client.send_message(message.channel, 'Cancellando...')
             async for msg in client.logs_from(message.channel):
                 await client.delete_message(msg)
