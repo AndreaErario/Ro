c273f8751270b9a5b553688a9bd677577dc3e5c8
AndreaErario <42212286+AndreaErario@users.noreply.github.com>
2018-08-16 15:37:38 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -40,7 +40,7 @@ async def on_message(message):
         embed.add_field(name="!setup", value="serve a preparare il server a essere utilizzato creando il ruolo \"Bot Admin\"\n", inline=False)
         embed.add_field(name="!help", value="Ormai dovresti saperlo :upside_down:\n", inline=False)
         embed.add_field(name="!cookie", value=":cookie:\n", inline=False)
-        embed.add_field(name="!lanciomoneta", value="Lancia una moneta e ti dice se è uscito Testa o Croce", inline=False)
+        embed.add_field(name="!lanciomoneta", value="Testa o Croce?", inline=False)
         embed.add_field(name="!say *", value="fai dire quello che vuoi al bot scrivendo la frase da fargli dire dopo il comando :speaking_head:\n", inline=False)
         embed.add_field(name="!clear *", value="Cancella moltissimi messaggi dalla chat \n(ATTENZIONE non potrai più tornare indietro!)\n", inline=False)
         embed.set_footer(text="I comandi affiancati da * possono essere usati solamente dalle persone che hanno il ruolo \"Bot Admin\"")
