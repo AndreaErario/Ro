c68488439897559bba52e056948143c834c0b826
Andrea Erario <42212286+AndreaErario@users.noreply.github.com>
2018-08-23 18:54:54 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -29,30 +29,35 @@ async def on_message(message):
         try:
             if "Bot Admin" in [role.name for role in message.server.roles]:
                 await client.send_message(message.channel, "Questo server è già pronto per essere usato.\nSe hai bisogno di aiuto scrivi il comando !help per una lista di comandi")
+            elif "Bot No" in [role.name for role in message.server.roles]:
+                await client.send_message(message.channel, "Non puoi fare nessun comando {}".format(message.author.name))
             else:
                 server = message.server
                 await client.create_role(server, name="Bot Admin")
-                await client.send_message(message.channel, "Un ruolo di nome Bot Admin è stato creato!\nSe sei il creatore del server devi assegnare quel ruolo a te stesso e a chi vuoi in modo che possiate usare i comandi speciali del bot\nScrivi il comando !help per una lista di comandi")
+                await client.send_message(message.channel, "Un ruolo di nome Bot Admin e uno di nome Bot No sono stati creati!\nSe sei il creatore del server devi assegnare il ruolo Bot Admin a te stesso e a chi vuoi in modo che possiate usare i comandi speciali del bot Se non vuoi che qualcuno possa usare i comandi del bot assegnali Bot No e non potrà più fare nulla\nScrivi il comando !help per una lista di comandi")
         except AttributeError:
             await client.send_message(message.author, "Questo comando funziona solo nei server")
     
     if message.content.upper().startswith("!HELP"):
-        embed = discord.Embed(
-            title = "Help",
-            description = "Ecco una lista di comandi che puoi usare con me:", 
-            colour = discord.Color.dark_blue()
-        )
-        embed.add_field(name="!setup", value="Serve a preparare il server a essere utilizzato creando il ruolo \"Bot Admin\"\n", inline=False)
-        embed.add_field(name="!help", value="Ormai dovresti saperlo :upside_down:\n", inline=False)
-        embed.add_field(name="!cookie", value=":cookie:\n", inline=False)
-        embed.add_field(name="!lanciomoneta", value="Testa o Croce?", inline=False)
-        embed.add_field(name="!say *", value="Fai dire quello che vuoi al bot scrivendo la frase da fargli dire dopo il comando :speaking_head:\n", inline=False)
-        embed.add_field(name="!clear *", value="Cancella moltissimi messaggi dalla chat \n(ATTENZIONE non potrai più tornare indietro!)\n", inline=False)
-        embed.add_field(name="!leave *", value="Elimina il ruolo \"Bot Admin\" e mi fa uscire dal Server\n", inline=False)
-        embed.add_field(name="!gif", value="Manda una gif con il tag che ci scrivi di seguito o ne cerca una completamente a caso su Giphy\n", inline=False)
-        embed.set_footer(text="I comandi affiancati da * possono essere usati solamente dalle persone che hanno il ruolo \"Bot Admin\"")
-        await client.send_message(message.channel, embed=embed)
-        await client.send_message(message.channel, "Se hai bisogno di maggiori informazioni puoi visitare il sito \nhttp://andreaerario.pythonanywhere.com/BotDiscord/Help")
+        if "Bot No" in [role.name for role in message.server.roles]:
+            await client.send_message(message.channel, "Non puoi fare nessun comando {}".format(message.author.name))
+        else:    
+            embed = discord.Embed(
+                title = "Help",
+                description = "Ecco una lista di comandi che puoi usare con me:", 
+                colour = discord.Color.dark_blue()
+            )
+            embed.add_field(name="!setup", value="Serve a preparare il server a essere utilizzato creando il ruolo \"Bot Admin\"\n", inline=False)
+            embed.add_field(name="!help", value="Ormai dovresti saperlo :upside_down:\n", inline=False)
+            embed.add_field(name="!cookie", value=":cookie:\n", inline=False)
+            embed.add_field(name="!lanciomoneta", value="Testa o Croce?", inline=False)
+            embed.add_field(name="!say *", value="Fai dire quello che vuoi al bot scrivendo la frase da fargli dire dopo il comando :speaking_head:\n", inline=False)
+            embed.add_field(name="!clear *", value="Cancella moltissimi messaggi dalla chat \n(ATTENZIONE non potrai più tornare indietro!)\n", inline=False)
+            embed.add_field(name="!leave *", value="Elimina il ruolo \"Bot Admin\" e mi fa uscire dal Server\n", inline=False)
+            embed.add_field(name="!gif", value="Manda una gif con il tag che ci scrivi di seguito o ne cerca una completamente a caso su Giphy\n", inline=False)
+            embed.set_footer(text="I comandi affiancati da * possono essere usati solamente dalle persone che hanno il ruolo \"Bot Admin\"")
+            await client.send_message(message.channel, embed=embed)
+            await client.send_message(message.channel, "Se hai bisogno di maggiori informazioni puoi visitare il sito \nhttp://andreaerario.pythonanywhere.com/BotDiscord/Help")
     
     
     if message.content.upper().startswith('!COOKIE'):
