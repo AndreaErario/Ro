65d05e33fe6e6aa0999bd76716238d96642feadc
Andrea Erario <42212286+AndreaErario@users.noreply.github.com>
2018-08-25 11:24:11 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -27,62 +27,39 @@ async def on_message(message):
     
     if message.content.upper().startswith("!SETUP"):
         try:
-            if "Bot No" in [role.name for role in message.author.roles]:
-                await client.send_message(message.channel, "Non puoi usare nessun comando {}".format(message.author.name))
-            elif "Bot Admin" in [role.name for role in message.server.roles] and "Bot No" in [role.name for role in message.server.roles]:
+            if "Bot Admin" in [role.name for role in message.server.roles]:
                 await client.send_message(message.channel, "Questo server è già pronto per essere usato.\nSe hai bisogno di aiuto scrivi il comando !help per una lista di comandi")
             else:
-                if "Bot Admin" in [role.name for role in message.server.roles]:
-                    server = message.server
-                    await client.create_role(server, name="Bot No")
-                    await client.send_message(message.channel, "Un ruolo di nome Bot Admin e uno di nome Bot No sono stati creati!\nSe sei il creatore del server devi assegnare il ruolo Bot Admin a te stesso e a chi vuoi in modo che possiate usare i comandi speciali del bot Se non vuoi che qualcuno possa usare i comandi del bot assegnali Bot No e non potrà più fare nulla\nScrivi il comando !help per una lista di comandi")
-                elif "Bot No" in [role.name for role in message.server.roles]:
-                    server = message.server
-                    await client.create_role(server, name="Bot Admin")
-                    await client.send_message(message.channel, "Un ruolo di nome Bot Admin e uno di nome Bot No sono stati creati!\nSe sei il creatore del server devi assegnare il ruolo Bot Admin a te stesso e a chi vuoi in modo che possiate usare i comandi speciali del bot Se non vuoi che qualcuno possa usare i comandi del bot assegnali Bot No e non potrà più fare nulla\nScrivi il comando !help per una lista di comandi")
-                else:
-                    server = message.server
-                    await client.create_role(server, name="Bot Admin")
-                    await client.create_role(server, name="Bot No")
-                    await client.send_message(message.channel, "Un ruolo di nome Bot Admin e uno di nome Bot No sono stati creati!\nSe sei il creatore del server devi assegnare il ruolo Bot Admin a te stesso e a chi vuoi in modo che possiate usare i comandi speciali del bot Se non vuoi che qualcuno possa usare i comandi del bot assegnali Bot No e non potrà più fare nulla\nScrivi il comando !help per una lista di comandi")
+                server = message.server
+                await client.create_role(server, name="Bot Admin")
+                await client.send_message(message.channel, "Un ruolo di nome Bot Admin e uno di nome Bot No sono stati creati!\nSe sei il creatore del server devi assegnare il ruolo Bot Admin a te stesso e a chi vuoi in modo che possiate usare i comandi speciali del bot\nScrivi il comando !help per una lista di comandi")
         except AttributeError:
-            await client.send_message(message.author, "Qualcosa non và :neutral_face:")
+            await client.send_message(message.author, "Questo comando funziona solo nei Server")
     
     if message.content.upper().startswith("!HELP"):
-        try:
-            if "Bot No" in [role.name for role in message.author.roles]:
-                await client.send_message(message.channel, "Non puoi usare nessun comando {}".format(message.author.name))
-            else:    
-                embed = discord.Embed(
-                    title = "Help",
-                    description = "Ecco una lista di comandi che puoi usare con me:", 
-                    colour = discord.Color.dark_blue()
-                )
-                embed.add_field(name="!setup", value="Serve a preparare il server a essere utilizzato creando il ruolo \"Bot Admin\"\n", inline=False)
-                embed.add_field(name="!help", value="Ormai dovresti saperlo :upside_down:\n", inline=False)
-                embed.add_field(name="!cookie", value=":cookie:\n", inline=False)
-                embed.add_field(name="!lanciomoneta", value="Testa o Croce?", inline=False)
-                embed.add_field(name="!say *", value="Fai dire quello che vuoi al bot scrivendo la frase da fargli dire dopo il comando :speaking_head:\n", inline=False)
-                embed.add_field(name="!clear *", value="Cancella moltissimi messaggi dalla chat \n(ATTENZIONE non potrai più tornare indietro!)\n", inline=False)
-                embed.add_field(name="!leave *", value="Elimina il ruolo \"Bot Admin\" e mi fa uscire dal Server\n", inline=False)
-                embed.add_field(name="!gif", value="Manda una gif con il tag che ci scrivi di seguito o ne cerca una completamente a caso su Giphy (nel caso dovessi inserire un Tag inesistente verrà mostrato un messaggio d'errore)\n", inline=False)
-                embed.add_field(name="Errori", value="Il Bot potrebbe mostrare un messaggio d'errore quando si usa nei messaggi diretti e quando qualcosa non và\n", inline=False)
-                embed.set_footer(text="I comandi affiancati da * possono essere usati solamente dalle persone che hanno il ruolo \"Bot Admin\"")
-                await client.send_message(message.channel, embed=embed)
-                await client.send_message(message.channel, "Se hai bisogno di maggiori informazioni puoi visitare il sito \nhttp://andreaerario.pythonanywhere.com/BotDiscord/Help")
-        except AttributeError:
-            await client.send_message(message.channel, "Qualcosa non và :neutral_face:")
+            embed = discord.Embed(
+                title = "Help",
+                description = "Ecco una lista di comandi che puoi usare con me:", 
+                colour = discord.Color.dark_blue()
+            )
+            embed.add_field(name="!setup", value="Serve a preparare il server a essere utilizzato creando il ruolo \"Bot Admin\"\n", inline=False)
+            embed.add_field(name="!help", value="Ormai dovresti saperlo :upside_down:\n", inline=False)
+            embed.add_field(name="!cookie", value=":cookie:\n", inline=False)
+            embed.add_field(name="!lanciomoneta", value="Testa o Croce?", inline=False)
+            embed.add_field(name="!say *", value="Fai dire quello che vuoi al bot scrivendo la frase da fargli dire dopo il comando :speaking_head:\n", inline=False)
+            embed.add_field(name="!clear *", value="Cancella moltissimi messaggi dalla chat \n(ATTENZIONE non potrai più tornare indietro!)\n", inline=False)
+            embed.add_field(name="!leave *", value="Elimina il ruolo \"Bot Admin\" e mi fa uscire dal Server\n", inline=False)
+            embed.add_field(name="!gif", value="Manda una gif con il tag che ci scrivi di seguito o ne cerca una completamente a caso su Giphy (nel caso dovessi inserire un Tag inesistente verrà mostrato un messaggio d'errore)\n", inline=False)
+            embed.add_field(name="Errori", value="Il Bot potrebbe mostrare un messaggio d'errore quando si usa nei messaggi diretti e quando qualcosa non và\n", inline=False)
+            embed.set_footer(text="I comandi affiancati da * possono essere usati solamente dalle persone che hanno il ruolo \"Bot Admin\"")
+            await client.send_message(message.channel, embed=embed)
+            await client.send_message(message.channel, "Se hai bisogno di maggiori informazioni puoi visitare il sito \nhttp://andreaerario.pythonanywhere.com/BotDiscord/Help")
     
     if message.content.upper().startswith('!COOKIE'):
         try:
-            if "Bot No" in [role.name for role in message.author.roles]:
-                await client.send_message(message.channel, "Non puoi usare nessun comando {}".format(message.author.name))
-            else:    
-                await client.send_message(message.channel, ":cookie:")
+            await client.send_message(message.channel, ":cookie:")
         except discord.errors.NotFound:
             return
-        except AttributeError:
-            await client.send_message(message.channel, "Qualcosa non và :neutral_face:")
     
     if message.content.upper().startswith('!SAY'):
         try:
@@ -93,12 +70,10 @@ async def on_message(message):
                     await client.send_message(message.channel,"%s " % (" ".join(args[1:])))
                 except discord.errors.NotFound:
                     return
-            elif "Bot No" in [role.name for role in message.author.roles]:
-                await client.send_message(message.channel, "Non puoi usare nessun comando {}".format(message.author.name))
             else:
                 await client.send_message(message.channel, "Scusa amico, non hai il permesso")
         except AttributeError:
-            await client.send_message(message.author, "Qualcosa non và :neutral_face:")
+            await client.send_message(message.author, "Questo comando funziona solo nei Server")
     
     if message.content.upper().startswith("!CLEAR"):
         try:
@@ -107,23 +82,15 @@ async def on_message(message):
                 async for msg in client.logs_from(message.channel):
                     await client.delete_message(msg)
                 await client.send_message(message.channel, 'Comando eseguito con successo! :wastebasket:')
-            elif "Bot No" in [role.name for role in message.author.roles]:
-                await client.send_message(message.channel, "Non puoi usare nessun comando {}".format(message.author.name))
             else:
                 await client.send_message(message.channel, 'Scusa amico, non hai il permesso')
         except AttributeError:
-            await client.send_message(message.author, "Qualcosa non và :neutral_face:")
+            await client.send_message(message.author, "Questo comando funziona solo nei Server")
     
     
     if message.content.upper().startswith("!LANCIOMONETA"):
-        try:
-            if "Bot No" in [role.name for role in message.author.roles]:
-                await client.send_message(message.channel, "Non puoi usare nessun comando {}".format(message.author.name))
-            else:
-                await client.send_message(message.channel, random.choice(["È uscito Testa", "È uscito Croce"]))
-        except AttributeError:
-            await client.send_message(message.channel, "Qualcosa non và :neutral_face:")
-    
+        await client.send_message(message.channel, random.choice(["È uscito Testa", "È uscito Croce"]))
+           
     
     if message.content.upper().startswith("!LEAVE"):
         try:
@@ -137,29 +104,24 @@ async def on_message(message):
                 else:
                     await client.send_message(message.channel, "Sono uscito dal Server")
                     await client.leave_server(message.server)
-            elif "Bot No" in [role.name for role in message.author.roles]:
-                await client.send_message(message.channel, "Non puoi usare nessun comando {}".format(message.author.name))
             else:
                 await client.send_message(message.channel, 'Scusa amico, non hai il permesso')
         except AttributeError:
-            await client.send_message(message.channel, "Qualcosa non và :neutral_face:")
+            await client.send_message(message.channel, "Questo comando funziona solo nei Server")
     
     if message.content.upper().startswith("!GIF"):
         try:
-            if "Bot No" in [role.name for role in message.author.roles]:
-                await client.send_message(message.channel, "Non puoi usare nessun comando {}".format(message.author.name))
+            gif_tag = message.content[5:]
+            rgif = g.random(tag=str(gif_tag))
+            response = requests.get(
+                str(rgif.get("data", {}).get('image_original_url')), stream=True
+            )
+            if gif_tag == "":
+                await client.send_message(message.channel, "Sto cercando...")
+                await client.send_file(message.channel, io.BytesIO(response.raw.read()), filename='video.gif', content="Ho preso una gif a caso su Giphy")
             else:
-                gif_tag = message.content[5:]
-                rgif = g.random(tag=str(gif_tag))
-                response = requests.get(
-                    str(rgif.get("data", {}).get('image_original_url')), stream=True
-                )
-                if gif_tag == "":
-                    await client.send_message(message.channel, "Sto cercando...")
-                    await client.send_file(message.channel, io.BytesIO(response.raw.read()), filename='video.gif', content="Ho preso una gif a caso su Giphy")
-                else:
-                    await client.send_message(message.channel, "Sto cercando...")
-                    await client.send_file(message.channel, io.BytesIO(response.raw.read()), filename='video.gif', content="Ho preso una gif a caso con il tag {} su Giphy".format(gif_tag))
+                await client.send_message(message.channel, "Sto cercando...")
+                await client.send_file(message.channel, io.BytesIO(response.raw.read()), filename='video.gif', content="Ho preso una gif a caso con il tag {} su Giphy".format(gif_tag))
         except AttributeError:
             await client.send_message(message.channel, "Qualcosa non và :neutral_face:")
         except discord.errors.HTTPException:
