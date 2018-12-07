417ed5083821d7cfa9577e5d2b15e828b7c6fefc
Andrea Erario <42212286+AndreaErario@users.noreply.github.com>
2018-12-07 14:40:23 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -253,7 +253,7 @@ async def on_message(message):
         if os.getenv("IDBOT") in [message.author.id]:
             await client.send_message(message.channel, "Perché mai dovrei usare un mio comando :thinking:")
         else:
-            await client.send_message(message.channel, random.choice(["Crocevia del Ciarpame","Passatempi Pomposi","Rapide Rischiose","Bosco Blaterante","Tempio Tomato","Bacino Bucato","Parco Pacifico","Montagnole Maledette","Spiagge Snob","Pinnacoli Pendenti","Sprofondo Stantio","Corso Commercio","Rifugio Ritirato","Borgo Bislacco","Condotti Confusi","Boschetto Bisunto","Laboratorio della Latrina","Approdo Avventurato","Lande Letali","Palmeto Paradisiaco"]))   
+            await client.send_message(message.channel, random.choice(["Crocevia del Ciarpame", "Montagnole Maledette", "Parco Pacifico", "Spiagge Snob", "Pinnacoli Pendenti", "Sponde del Saccheggio", "Condotti Confusi", "Picco Polare", "Rampe Ghiacciate", "Borgallegro", "Approdo Avventurato", "Lande Letali", "Borgo Bislacco", "Sprofondo Stantio", "Corso Commercio", "Tempio Tomato", "Palmeto Paradisiaco", "Rifugio Ritirato", "Bosco Blaterante", "Rapide Rischiose", "Passatempi Pomposi"]))   
      
     if message.content.upper().startswith("!REVERSE"):
         if os.getenv("IDBOT") in [message.author.id]:
