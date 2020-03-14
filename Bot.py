5a9a1a733d2ef1541e4f5ea7f0b16f3bde9b8e15
AndreaErario <andreaerario04@gmail.com>
2020-03-14 19:31:34 +0100

    Added command !coronavirus

--- Bot.py
+++ Bot.py
@@ -21,10 +21,29 @@ async def on_message(message):
             colour = discord.Color.teal(),
             )
         embed.add_field(name="!help", value="Dovresti averlo capito :smirk:", inline=False)
+        embed.add_field(name="!coronavirus", value="Fornisce i dati globali e italiani sul contagio del coronavirus", inline=False)
         embed.add_field(name="!ciao", value="Un piccolo gesto che mi rende felice :blush:", inline=False)
         embed.add_field(name="!lanciamoneta", value="Lancio una moneta, testa o croce?", inline=False)
         embed.add_field(name="!gif", value="Permettimi di cercare una Gif che possa cambiarti la giornata!", inline=False)
         await  message.channel.send(embed=embed)
+
+    if message.content.upper().startswith("!CORONAVIRUS"):
+        dati_totali = requests.get("https://coronavirus-tracker-api.herokuapp.com/all")
+        dati_totali = dati_totali.json()
+        totale_confermati = dati_totali["confirmed"]["latest"]
+        totale_guariti = dati_totali["recovered"]["latest"]
+        totale_decessi = dati_totali["deaths"]["latest"]
+        confermati_italia = dati_totali["confirmed"]["locations"][16]["latest"]
+        guariti_italia = dati_totali["recovered"]["locations"][16]["latest"]
+        decessi_italia = dati_totali["deaths"]["locations"][16]["latest"]
+        embed = discord.Embed(
+            title = "Coronavirus", 
+            description = "",
+            colour = discord.Color.red(),
+            )
+        embed.add_field(name="Nel mondo", value=f"Totale casi confermati: {totale_confermati}\nGuariti: {totale_guariti}\nDecessi: {totale_decessi}", inline=False)
+        embed.add_field(name="In Italia", value=f"Totale casi confermati: {confermati_italia}\nGuariti: {guariti_italia}\nDecessi: {decessi_italia}", inline=False)
+        await message.channel.send(embed=embed)
 
     if message.content.upper().startswith("!CIAO"):
         await message.channel.send("Ciao :wave:")
@@ -33,15 +52,16 @@ async def on_message(message):
         await message.channel.send(random.choice(["È uscito Testa", "È uscito Croce"]))
 
     if message.content.upper().startswith("!GIF"):
+        chiave = os.getenv("GIPHYKEY")
         try:
             gif_tag = message.content[5:]
             if gif_tag == "":
-                gif = requests.get("https://api.giphy.com/v1/gifs/random?api_key={}&tag=&rating=G".format(os.getenv("GIPHYKEY")))
+                gif = requests.get(f"https://api.giphy.com/v1/gifs/random?api_key={chiave}&tag=&rating=G")
                 gif = gif.json()
                 gif = gif["data"]["image_url"]
                 await  message.channel.send(f"Ecco a te una gif a caso presa da Giphy\n{gif}")
             else:
-                gif = requests.get("https://api.giphy.com/v1/gifs/random?api_key={}&tag={}&rating=G".format(os.getenv("GIPHYKEY"), gif_tag))
+                gif = requests.get(f"https://api.giphy.com/v1/gifs/random?api_key={chiave}&tag={gif_tag}&rating=G")
                 gif = gif.json()
                 gif = gif["data"]["image_url"]
                 await message.channel.send(f"Ecco a te una gif a caso con il tag {gif_tag} su Giphy\n{gif}")
